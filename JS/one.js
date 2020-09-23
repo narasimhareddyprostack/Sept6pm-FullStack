ccf59cdc41980de6204b37ccf3684fdb1b87bdc8
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2020-09-23 18:47:32 +0530

    if

--- JS/one.js
+++ JS/one.js
@@ -1,3 +1,16 @@
-var a = true;
-console.log(a);
-document.write(a)
+var a = 1
+var b = 2
+var sathis_1 = a>b
+var bool =false;
+console.log(bool)
+console.log('*********************')
+console.log(sathis_1)
+if(1){
+    console.log('True Block')
+    console.log(a)
+}
+else
+{
+console.log('False Block')
+console.log(b)
+}
